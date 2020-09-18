f9618727b3a4e260f4333b7082a8257d144f3bba
Yona <uzicandi@gmail.com>
2020-09-19 08:08:10 +0900

    navigation bar styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,49 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Movies from './Movies';
 import Home from './Home';
+import styled from 'styled-components';
 
-const App = () => {
+const Navigator = styled.div`
+  .links {
+    display: flex;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    background: #dfdfdf;
+    width: 100%;
+    height: 60px;
+  }
+  .link {
+    list-style: none;
+    margin-left: 30px;
+  }
+  .link a {
+    text-decoration: none;
+    color: black;
+  }
+  .link a:hover {
+    color: #3b88c3;
+  }
+`;
+
+function App() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/movies">소개</Link>
-        </li>
-      </ul>
-      <hr />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/movies" component={Movies} />
-    </div>
+    <>
+      <Navigator>
+        <ul class="links">
+          <li class="link">
+            <Link to="/">Home</Link>
+          </li>
+          <li class="link">
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/movies" component={Movies} />
+      </Navigator>
+    </>
   );
-};
+}
 
 export default App;
