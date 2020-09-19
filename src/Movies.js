285e798d123d4e9833c50311318c992a6a3a1a40
Yona <uzicandi@gmail.com>
2020-09-19 11:06:28 +0900

    2020-09-19 Movie List Styling

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Movie from './components/Movie';
+import styled from 'styled-components';
+
+const MovieList = styled.div`
+  .movies {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 50px;
+    padding-top: 70px;
+    width: 100%;
+  }
+`;
+
+const useScroll = () => {
+  const [limit, setLimit] = useState(4);
+  const onScroll = () => {
+    setLimit(limit + 4);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+  return limit;
+};
 
 function Movies() {
   const [movies, setMovies] = useState(null);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
-  //const [page, setPage] = useState(1);
+  const limit = useScroll();
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -13,8 +40,9 @@ function Movies() {
         setError(null);
         setMovies(null);
         setLoading(true);
+        console.log('limit2 ', limit);
         const response = await axios.get(
-          'https://yts.mx/api/v2/list_movies.json?limit=4'
+          `https://yts.mx/api/v2/list_movies.json?limit=${limit}`
         );
         setMovies(response.data.data.movies);
       } catch (e) {
@@ -24,26 +52,42 @@ function Movies() {
     };
     fetchMovies();
   }, []);
-
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러발생</div>;
   if (!movies) return null;
+
   console.log('movies', movies);
 
-  window.scroll(() => {
-    console.log('scroll');
-    // if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-    //   this.fetchMovies();
-    // }
-  });
-
   return (
-    <ul className="main">
-      {movies.map(movie => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-    </ul>
+    <>
+      <MovieList>
+        <ul className="movies">
+          {movies.map(movie => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </ul>
+      </MovieList>
+    </>
   );
 }
 
 export default Movies;
+
+// const fetchMovies = async () => {
+//   const {
+//     data: {
+//       data: { movies }
+//     }
+//   } = await axios.get(
+//     `https://yts.mx/api/v2/list_movies.json?limit=${limit}`
+//   );
+//   return setMovies({ movies });
+// };
---
+++ src/components/Movie.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Movie.css';
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        {/* <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul> */}
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
