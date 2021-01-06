12b13289c68b82b9fa3f741bc87e2ee9254568f6
Yona <uzicandi@gmail.com>
2021-01-06 22:43:34 +0900

    Updates

---
+++ precache-manifest.cb6965a64b45a160f65b1f0a7c860299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1382e5f1e7fa1dc2cb6a078077d720",
+    "url": "/introduce-movie-app/index.html"
+  },
+  {
+    "revision": "cff98f941b6a6d25fd9a",
+    "url": "/introduce-movie-app/static/css/main.779247b9.chunk.css"
+  },
+  {
+    "revision": "f997b11936719ad9c9d1",
+    "url": "/introduce-movie-app/static/js/2.d30212be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/introduce-movie-app/static/js/2.d30212be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff98f941b6a6d25fd9a",
+    "url": "/introduce-movie-app/static/js/main.84fecc64.chunk.js"
+  },
+  {
+    "revision": "670cb8201548e250ce0c",
+    "url": "/introduce-movie-app/static/js/runtime-main.0f257a3c.js"
+  }
+]);
